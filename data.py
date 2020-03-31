86c9c8697939fd18e8770f172164c7a3415b0242
GiannhsAravantinos <johnnykef@hotmail.com>
2020-03-31 20:22:40 +0300

    5

--- data.py
+++ data.py
@@ -23,9 +23,9 @@ hasReceivedGenesisBlock=False
 connectedParticipants=0
 
 #αλλάζουμε αυτες τις τιμές για να παραμετροποι΄ήσουμε το συστημά μας
-numOfParticipants=3
-capacity=2
-difficulty=5
+numOfParticipants=5
+capacity=1
+difficulty=4
 
 lock = threading.RLock()# ώστε καθε νημα που έχει προσβαση στις ανώτερες τιμές να εχει ατομική προσβαση
 
