6962273badcf79974b2a008e71ca752bc8cd6a5d
GiannhsAravantinos <johnnykef@hotmail.com>
2020-04-02 13:19:02 +0300

    asd

--- data.py
+++ data.py
@@ -25,7 +25,7 @@ connectedParticipants=0
 
 #αλλάζουμε αυτες τις τιμές για να παραμετροποι΄ήσουμε το συστημά μας
 numOfParticipants=5
-capacity=5
+capacity=1
 difficulty=5
 
 lock = threading.RLock()# ώστε καθε νημα που έχει προσβαση στις ανώτερες τιμές να εχει ατομική προσβαση
