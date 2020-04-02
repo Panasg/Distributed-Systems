1ef63de9b477ddc7593a83efcc8746c5534749c5
GiannhsAravantinos <johnnykef@hotmail.com>
2020-04-02 12:04:22 +0300

    dif 5

--- data.py
+++ data.py
@@ -26,7 +26,7 @@ connectedParticipants=0
 #αλλάζουμε αυτες τις τιμές για να παραμετροποι΄ήσουμε το συστημά μας
 numOfParticipants=5
 capacity=10
-difficulty=4
+difficulty=5
 
 lock = threading.RLock()# ώστε καθε νημα που έχει προσβαση στις ανώτερες τιμές να εχει ατομική προσβαση
 
